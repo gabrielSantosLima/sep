e2657fa03c1ee96d7faa20a9fbb81f29bdf0ced8
Gabriel Lima <bielsantoslima10@gmail.com>
2020-11-23 18:31:07 -0400

    Adicionado propriedades de formatação do componente de tabela de dados

--- src/components/DataTable/styles.js
+++ src/components/DataTable/styles.js
@@ -1,19 +1,34 @@
 import styled from 'styled-components';
 
+const isScrolled = `
+    tbody{
+        display: block;
+        max-height: 360px;
+        overflow-y: auto;
+    }
+
+    thead, tbody tr {
+        display: table;
+        width: 100%;
+        table-layout: fixed;
+    }
+`;
+
 export const Container = styled.table`
     border: 1px solid var( --color-body);
     margin: 8px 0px;
-    width: 80%;
-    
+    width: ${props => props.isFullWidth ? "100%" : "80%"};
+
+    ${props => props.isScrolled ? isScrolled : ""}
+
     th{
         font-size: 1.4rem;
-        top: 0;
         background-color: var(--color-title-table);
     }
     td{
-        font-size: 1.1rem;
+        font-size: 1.2rem;
     }
-    
+
     th, td{
         text-align: center;
         padding: 8px 15px 8px 15px;
