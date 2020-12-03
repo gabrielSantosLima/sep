4f250ad4a8026eb6da084f453ee88a93e3fc83ba
Gabriel Lima <bielsantoslima10@gmail.com>
2020-12-03 00:39:16 -0400

    Adicionado SWR e Tela de Carregamento

--- src/components/ContainerMain/styles.js
+++ src/components/ContainerMain/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   margin: auto;
   margin-top: 0;
   width: 75%;
+  min-height: 100%;
   display: flex;
   flex-direction: column;
   background-color: var(--color-container);
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import { useFetch } from './../../services/useFetch'
+
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import ContainerMain from '../../components/ContainerMain'
 import Main from '../../components/Main'
 import TreeBar from '../../components/TreeBar';
+import Loading from '../../components/Loading';
 
 import { Container } from './styles';
 
 const Homepage = () => {
+  const { data, error } = useFetch('/alunos')
+
+  if(error) return <h1>Deu capim</h1>  
+  if(!data) return <Loading />  
 
   return (
     <Container>
@@ -21,6 +28,9 @@ const Homepage = () => {
         </TreeBar>
           
         <Main>
+          {
+            data.map(aluno => <li key={aluno.id}>{aluno.nome}</li>)
+          }
         </Main>
           
         <Footer />
